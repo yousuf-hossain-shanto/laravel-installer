d58f42800cd5c7a2018817b2f00b7f9fc0dc6ff2
Yousuf Hossain <yousuf.hossain.shanto@gmail.com>
2020-12-18 08:24:19 +0600

    Added Envato license file

--- src/Helpers/EnvironmentManager.php
+++ src/Helpers/EnvironmentManager.php
@@ -34,7 +34,7 @@ class EnvironmentManager
      */
     public function getEnvContent()
     {
-        if (! file_exists($this->envPath)) {
+        if (!file_exists($this->envPath)) {
             if (file_exists($this->envExamplePath)) {
                 copy($this->envExamplePath, $this->envPath);
             } else {
@@ -95,21 +95,21 @@ class EnvironmentManager
         $results = trans('installer_messages.environment.success');
 
         $envFileData =
-        'APP_NAME=\''.$request->app_name."'\n".
-        'APP_ENV='.$request->environment."\n".
-        'APP_KEY='.'base64:'.base64_encode(Str::random(32))."\n".
-        'APP_DEBUG='.$request->app_debug."\n".
-        'APP_URL='.$request->app_url."\n\n".
-        'DB_CONNECTION='.$request->database_connection."\n".
-        'DB_HOST='.$request->database_hostname."\n".
-        'DB_PORT='.$request->database_port."\n".
-        'DB_DATABASE='.$request->database_name."\n".
-        'DB_USERNAME='.$request->database_username."\n".
-        'DB_PASSWORD='.$request->database_password."\n\n".
-        'ENVATO_PURCHASE_CODE=\''.$request->envato_purchase_code."'\n";
+            'APP_NAME=\'' . $request->app_name . "'\n" .
+            'APP_ENV=' . $request->environment . "\n" .
+            'APP_KEY=' . 'base64:' . base64_encode(Str::random(32)) . "\n" .
+            'APP_DEBUG=' . $request->app_debug . "\n" .
+            'APP_URL=' . $request->app_url . "\n\n" .
+            'DB_CONNECTION=' . $request->database_connection . "\n" .
+            'DB_HOST=' . $request->database_hostname . "\n" .
+            'DB_PORT=' . $request->database_port . "\n" .
+            'DB_DATABASE=' . $request->database_name . "\n" .
+            'DB_USERNAME=' . $request->database_username . "\n" .
+            'DB_PASSWORD=' . $request->database_password . "\n\n";
 
         try {
             file_put_contents($this->envPath, $envFileData);
+            file_put_contents(storage_path('.envato'), json_encode(['license' => $request->envato_purchase_code]));
         } catch (Exception $e) {
             $results = trans('installer_messages.environment.errors');
         }
